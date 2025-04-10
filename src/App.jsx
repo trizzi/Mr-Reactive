a55ee9f029a69d76961c9eba61d44124b1721922
trizzi <patrickakhamiogu@gmail.com>
2025-04-10 10:14:19 +0100

    Initial commit for mr-reactive

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import Showcase from './components/Showcase';
+import Reviews from './components/Reviews';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Showcase />
+			<Reviews />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
